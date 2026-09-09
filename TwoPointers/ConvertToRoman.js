8c2421f2931177f48d0735184f3a98273dc12789
Ubaid_Bangash <ubaidkhan0077800@gmail.com>
2026-09-09 18:42:33 -0400

    Implement function to convert numbers to Roman numerals

---
+++ TwoPointers/ConvertToRoman.js
@@ -0,0 +1,28 @@
+function convertToRoman(num) {
+  let result = '';
+  const romanArr = [
+    ["M",	1000],
+    ["CM",	900],
+    ["D",	500],
+    ["CD",	400],
+    ["C",	100],
+    ["XC",	90],
+    ["L",	50],
+    ["XL",	40],
+    ["X",	10],
+    ["IX",	9],
+    ["V",	5],
+    ["IV",	4],
+    ["I",	1],
+  ]
+
+  for(let[roman, value] of romanArr){
+     while(num >= value){
+         result += roman;
+         num -= value;
+     }
+  }
+ return result;
+}
+
+convertToRoman(36);
